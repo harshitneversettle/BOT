7a1b7f3ed1929bb43c082e96469bebc7db3c21d8
Harshit yadav <harshityadav5499@gmail.com>
2025-12-07 21:42:34 +0530

    changed model

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 const brain = fs.readFileSync("brain.txt", "utf8");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 
 async function getAIResponse(userMessage) {
   try {
